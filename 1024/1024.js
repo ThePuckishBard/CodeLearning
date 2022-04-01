6771053693e030eac4c807fcc1558b246c9cabcb
ThePuckishBard <101846174+ThePuckishBard@users.noreply.github.com>
2022-04-01 18:42:53 -0400

    Added internal field
    added a field and function to add new numbers, and fixed redraw and key press event handler.

--- 1024/1024.js
+++ 1024/1024.js
@@ -1,25 +1,47 @@
 console.log('hello');
-
+var field = new Array (4);
+for(var j = 0; j < 4; j++){
+    field[j] = new Array(4);
+}
 
 window.addEventListener("load", init);
+//redraw();
+window.addEventListener("keydown", keyPress);
 function init() {
-    addNumber(Math.floor((Math.random() * 16) + 1));
+    field[0][0] = 2;
+    redraw();
+    //putNumber(Math.floor(Math.random() * 15), 2);
 }
 
 
-function addNumber(number) {
-    var tile = document.getElementById('tile' + number);
-
-    tile.innerHTML = '2';
-    
-}
-
-function keyPress() {
-    if (event.key === 'ArrowDown') {
-        console.log('hello');
+function putNumber(tileNumber, value) {
+    var tile = document.getElementById('tile' + tileNumber);
+    console.log('tile' + tileNumber);
+    if (!value) {
+        tile.innerHTML = '' ;         
+    }else{    
+        tile.innerHTML = value;
     }
 }
 
-function redraw(){
+function keyPress(event) {
+   // if (event === 'ArrowDown') { 
+   //}not working
+        console.log('hello');
+        addNewNumber(2);
+    redraw();
+}
+
+function redraw() {
+   for(var i = 0; i < 4; i++){
+       for(var j = 0; j < 4; j++){
+           putNumber('' + i + j, field[i][j]);
+       }
+   }
+}
+
+
+function addNewNumber(number){
     
+    //every time you call NewNumber a new number only apears in an empty cell
 }
