ebcc171ace27038e0d5c082f12caff3d2fe4d96c
ThePuckishBard <101846174+ThePuckishBard@users.noreply.github.com>
2022-04-01 17:08:06 -0400

    Small Addition
    the beginning or redraw and key press. if they're even gonna be useful later.

--- 1024/1024.js
+++ 1024/1024.js
@@ -2,15 +2,24 @@ console.log('hello');
 
 
 window.addEventListener("load", init);
-
 function init() {
     addNumber(Math.floor((Math.random() * 16) + 1));
 }
-function addNumber(number){
+
+
+function addNumber(number) {
     var tile = document.getElementById('tile' + number);
 
     tile.innerHTML = '2';
+    
 }
 
+function keyPress() {
+    if (event.key === 'ArrowDown') {
+        console.log('hello');
+    }
+}
 
-       
+function redraw(){
+    
+}
