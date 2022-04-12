6de8c27f0664593c8971390852dc6408e629ed45
ThePuckishBard <101846174+ThePuckishBard@users.noreply.github.com>
2022-04-12 18:22:36 -0400

    function keyPress now is now complete.

--- 1024/1024.js
+++ 1024/1024.js
@@ -7,22 +7,22 @@ window.addEventListener("load", init);
 //redraw();
 window.addEventListener("keydown", keyPress);
 function init() {
-    field[0][3] = 2;
-    /*field[0][2] = 4;
-    field[0][1] = 2;
-    field[0][0] = 2;
-    field[1][3] = 2;
-    field[1][2] = 4;
-    field[1][1] = 2;
-    field[1][0] = 2;
-    field[2][3] = 2;
-    field[2][2] = 4;
-    field[2][1] = 2;
-    field[2][0] = 2;
-    field[3][3] = 2;
-    field[3][2] = 4;
-    field[3][1] = 2;
-    field[3][0] = 2;*/
+    addNewNumber(2);
+    addNewNumber(2);
+    addNewNumber(4);
+     /*field[0][0] = 2;
+     field[1][3] = 2;
+     field[1][2] = 4;
+     field[1][1] = 2;
+     field[1][0] = 2;
+     field[2][3] = 2;
+     field[2][2] = 4;
+     field[2][1] = 2;
+     field[2][0] = 2;
+     field[3][3] = 2;
+     field[3][2] = 4;
+     field[3][1] = 2;
+     field[3][0] = 2;*/
     redraw();
     //putNumber(Math.floor(Math.random() * 15), 2);
 }
@@ -39,7 +39,7 @@ function putNumber(tileNumber, value) {
 }
 
 function keyPress(event) {
-    var t;
+    var t, changed = false;
     console.log('hello');
     if (event.key === 'ArrowLeft') {//move tiles to the left
         for (var i = 0; i < 4; i++) {
@@ -47,17 +47,19 @@ function keyPress(event) {
             for (var j = 1; j < 4 && t < 4; ) {
                 if (field[i][j]) {
                     if (!field[i][t]) {
+                        changed = true;
                         field[i][t] = field[i][j];
                         field[i][j] = 0;
                         j++;
                     } else if (field[i][j] === field[i][t]) {
+                        changed = true;
                         field[i][t] = field[i][t] * 2;
                         field[i][j] = 0;
                         j++;
                         t++;
                     } else {
                         t++;
-                        if (t >= j){
+                        if (t >= j) {
                             j++;
                         }
                     }
@@ -67,23 +69,82 @@ function keyPress(event) {
             }
         }
     }
-    if (event.key === 'ArrowRight') {//move tiles to the left
+    if (event.key === 'ArrowRight') {//move tiles to the right
+        for (var i = 0; i < 4; i++) {
+            t = 3;
+            for (var j = 2; j > -1 && t > -1; ) {
+                if (field[i][j]) {
+                    if (!field[i][t]) {
+                        changed = true;
+                        field[i][t] = field[i][j];
+                        field[i][j] = 0;
+                        j--;
+                    } else if (field[i][j] === field[i][t]) {
+                        changed = true;
+                        field[i][t] = field[i][t] * 2;
+                        field[i][j] = 0;
+                        j--;
+                        t--;
+                    } else {
+                        t--;
+                        if (t <= j) {
+                            j--;
+                        }
+                    }
+                } else {
+                    j--;
+                }
+            }
+        }
+    }
+  
+     if (event.key === 'ArrowDown') {
+         for (var i = 0; i < 4; i++) {
+            t = 3;
+            for (var j = 2; j > -1 && t > -1; ) {
+                if (field[j][i]) {
+                    if (!field[t][i]) {
+                        changed = true;
+                        field[t][i] = field[j][i];
+                        field[j][i] = 0;
+                        j--;
+                    } else if (field[j][i] === field[t][i]) {
+                        changed = true;
+                        field[t][i] = field[t][i] * 2;
+                        field[j][i] = 0;
+                        j--;
+                        t--;
+                    } else {
+                        t--;
+                        if (t <= j) {
+                            j--;
+                        }
+                    }
+                } else {
+                    j--;
+                }
+            }
+        }
+     }
+     if (event.key === 'ArrowUp') {//move tiles to the left
         for (var i = 0; i < 4; i++) {
             t = 0;
             for (var j = 1; j < 4 && t < 4; ) {
-                if (field[i][j]) {
-                    if (!field[i][t]) {
-                        field[i][t] = field[i][j];
-                        field[i][j] = 0;
+                if (field[j][i]) {
+                    if (!field[t][i]) {
+                        changed = true;
+                        field[t][i] = field[j][i];
+                        field[j][i] = 0;
                         j++;
-                    } else if (field[i][j] === field[i][t]) {
-                        field[i][t] = field[i][t] * 2;
-                        field[i][j] = 0;
+                    } else if (field[j][i] === field[t][i]) {
+                        changed = true;
+                        field[t][i] = field[t][i] * 2;
+                        field[j][i] = 0;
                         j++;
                         t++;
                     } else {
                         t++;
-                        if (t >= j){
+                        if (t >= j) {
                             j++;
                         }
                     }
@@ -93,8 +154,10 @@ function keyPress(event) {
             }
         }
     }
-addNewNumber(2);
+   if (changed) {
+        addNewNumber(2);
         redraw();
+    }    
 }
 
 function redraw() {
